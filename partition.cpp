c3506afd2ee0d6095b6e3b9d72f03a2dad68c8da
me-cafebabe <me_cafebabe@hotmail.com>
2022-07-28 03:45:22 +0800

    partition: Mount partition as RO when updating size
    So that read-only partitions can also get mounted.
    
    Change-Id: I736a738963ba208cfdeaadc05c47d45b1bb7bf3e
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- partition.cpp
+++ partition.cpp
@@ -3083,12 +3083,15 @@ bool TWPartition::Restore_Image(PartitionSettings *part_settings) {
 }
 
 bool TWPartition::Update_Size(bool Display_Error) {
-	bool ret = false, Was_Already_Mounted = false;
+	bool ret = false, Was_Already_Mounted = false, ro = false;
 
 	Find_Actual_Block_Device();
 
 	if (Actual_Block_Device.empty())
 		return false;
+
+	ro = Mount_Read_Only;
+	Mount_Read_Only = true;
 
 	if (!Can_Be_Mounted && !Is_Encrypted) {
 		if (TWFunc::Path_Exists(Actual_Block_Device) && Find_Partition_Size()) {
@@ -3096,7 +3099,7 @@ bool TWPartition::Update_Size(bool Display_Error) {
 			Backup_Size = Size;
 			return true;
 		}
-		return false;
+		goto fail;
 	}
 
 	Was_Already_Mounted = Is_Mounted();
@@ -3105,14 +3108,14 @@ bool TWPartition::Update_Size(bool Display_Error) {
 		if (!Mount(false))
 			return true;
 	} else if (!Mount(Display_Error))
-		return false;
+		goto fail;
 
 	ret = Get_Size_Via_statfs(Display_Error);
 	if (!ret || Size == 0) {
 		if (!Get_Size_Via_df(Display_Error)) {
 			if (!Was_Already_Mounted)
 				UnMount(false);
-			return false;
+			goto fail;
 		}
 	}
 
@@ -3126,7 +3129,7 @@ bool TWPartition::Update_Size(bool Display_Error) {
 		} else {
 			if (!Was_Already_Mounted)
 				UnMount(false);
-			return false;
+			goto fail;
 		}
 	} else if (Has_Android_Secure) {
 		if (Mount(Display_Error))
@@ -3134,12 +3137,16 @@ bool TWPartition::Update_Size(bool Display_Error) {
 		else {
 			if (!Was_Already_Mounted)
 				UnMount(false);
-			return false;
+			goto fail;
 		}
 	}
 	if (!Was_Already_Mounted)
 		UnMount(false);
+	Mount_Read_Only = ro;
 	return true;
+fail:
+	Mount_Read_Only = ro;
+	return false;
 }
 
 bool TWPartition::Find_Wildcard_Block_Devices(const string& Device) {
